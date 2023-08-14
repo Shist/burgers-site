c766a348535d0d61786174160d35a3103b2e858e
Shist <shist.pupust@mail.ru>
2023-08-14 04:40:33 +0300

    Added header layout and its styles, added fonts to the project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,40 @@
 import st from "./Header.module.scss";
+import logoText from "../../images/header/logo-text.svg";
+import logoImage from "../../images/header/logo-img.svg";
+import burgerImage from "../../images/header/burger.png";
 
 function Header() {
-  return <header className={st.header}></header>;
+  return (
+    <header className={st.header}>
+      <div className={st["header__logo-wrapper"]}>
+        <img src={logoText} alt="Your meal" className="header__logo-text" />
+        <img
+          src={logoImage}
+          alt="Your meal logo"
+          className="header__logo-image"
+        />
+      </div>
+      <div className={st["header__main-headline-wrapper"]}>
+        <img
+          src={burgerImage}
+          alt="Burger"
+          className={st["header__burger-img"]}
+        />
+        <div className={st["header__headline-text-wrapper"]}>
+          <h1 className={st["header__main-headline"]}>
+            Только самые{" "}
+            <span className={st["header__main-headline_orange"]}>
+              сочные бургеры!
+            </span>
+          </h1>
+          <span className={st["header__free-delivery-text"]}>
+            Бесплатная доставка от{" "}
+            <span className={st["header__free-delivery-text_bold"]}>599₽</span>
+          </span>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
