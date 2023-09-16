86a3a7c126fa59b736f5404947dde9d75e7f098e
Shist <shist.pupust@mail.ru>
2023-09-16 06:25:49 +0300

    Added timeout (0.1 sec) before post-request-spinner appears + project build

--- src/components/BottomFixedSpinner/BottomFixedSpinner.jsx
+++ src/components/BottomFixedSpinner/BottomFixedSpinner.jsx
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react";
+
 import st from "./BottomFixedSpinner.module.scss";
 import "./../../styles/template.scss";
 
 function BottomFixedSpinner({ dataIsSending }) {
+  const [display, setDisplay] = useState("hidden-element");
+
+  useEffect(() => {
+    let timerId = null;
+    if (dataIsSending) {
+      timerId = setTimeout(() => {
+        setDisplay("appeared-flex");
+      }, 100);
+    } else {
+      setDisplay("hidden-element");
+    }
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [dataIsSending]);
+
   return (
-    <div
-      className={`${st["bottom-fixed-spinner"]} ${
-        dataIsSending ? "appeared-flex" : "hidden-element"
-      }`}
-    >
+    <div className={`${st["bottom-fixed-spinner"]} ${display}`}>
       <h2 className={st["bottom-fixed-spinner__headline"]}>
         Отправка данных на сервер
       </h2>
